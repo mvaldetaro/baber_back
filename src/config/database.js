d0909ff1838223e93773ff167869bda10e8bdbef
Magno Valdetaro <mvaldetaro@gmail.com>
2020-02-11 07:56:00 -0300

    Conexão com o banco e cadsatro de usuário

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'gobarbar',
+  database: 'gobarber',
   define: {
     timestamps: true,
     underscored: true,
