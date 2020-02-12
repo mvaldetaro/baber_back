1b17e22833f7386e2d966b0b8f63b424e7c4d885
Magno Valdetaro <mvaldetaro@gmail.com>
2020-02-12 08:03:19 -0300

    Implementa middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+/** Rotas de Abertas */
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+/** Rotas de Privadas */
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+
 
 export default routes;
