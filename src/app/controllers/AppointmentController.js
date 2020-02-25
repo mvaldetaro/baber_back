7d1c789928bede602dfd2c98a4c1b031823d2eac
Magno Valdetaro <mvaldetaro@gmail.com>
2020-02-25 09:17:40 -0300

    Envia email de cancelamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import File from '../models/File';
 import User from '../models/User';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -84,45 +86,58 @@ class AppointmentController {
       date: hourStart,
     });
 
-    const user =  await User.findByPk(req.userId)
+    const user = await User.findByPk(req.userId);
     const formattedDate = format(
       hourStart,
       "'dia' dd 'de' MMMM', às' H:mm'h'",
       { locale: pt }
-    )
+    );
 
     // Notify provider
     await Notification.create({
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
-    })
+    });
 
     return res.json(appointment);
   }
 
   async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
-    const appointment =  await Appointment.findByPk(req.params.id);
-
-    if(appointment.user_id !== req.userId) {
+    if (appointment.user_id !== req.userId) {
       return res.status(401).json({
-        error: "You don't have permission to cancel this appointment."
-      })
+        error: "You don't have permission to cancel this appointment.",
+      });
     }
 
-    const dateWithSub = subHours(appointment.date, 2)
+    const dateWithSub = subHours(appointment.date, 2);
 
-    if(isBefore(dateWithSub, new Date())) {
+    if (isBefore(dateWithSub, new Date())) {
       return res.status(401).json({
-        error: 'You can only cancel appointments 2 hours in advance.'
+        error: 'You can only cancel appointments 2 hours in advance.',
       });
     }
 
     appointment.canceled_at = new Date();
 
-    await appointment.save(appointment);
+    await appointment.save();
 
-    return res.json()
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem novo cancelamento',
+    });
+
+    return res.json(appointment);
   }
 }
 
---
+++ src/config/mail.js
@@ -0,0 +1,15 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '0b85ce1d39b264',
+    pass: '04083a80d93196'
+  },
+  default: {
+    from: 'Magno Valdetaro <noreply@gobarber.com>'
+  }
+}
+
+// Amazon SES --> produção
+// MailTrap --> desenvolvimento
